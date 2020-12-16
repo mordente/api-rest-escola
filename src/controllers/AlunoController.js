78d8c224a7fcb83a99710422de8117f5b20a7f33
Mordente <revolutionms3@hotmail.com>
2020-12-16 17:37:20 -0300

    alunos crud

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -3,8 +3,104 @@ import Aluno from '../models/Aluno';
 class AlunoController {
   async index(req, res) {
     const alunos = await Aluno.findAll();
-    res.json(alunos);
+    return res.json(alunos);
   }
+
+  async store (req, res) {
+    try {
+      const novoAluno = await Aluno.create(req.body);
+      return res.json(novoAluno)
+    } catch(e) {
+      res.status(400).json({
+        errors: e.errors.map((err) => err.message)
+      });
+    }
+
+  }
+
+  async retrieve(req, res) {
+    try {
+      const { id } = req.params;
+
+      if(!id) {
+        return res.status(401).json({
+          errors: ['ID não enviado.']
+        })
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if(!aluno) {
+        return res.status(401).json({
+          errors: ['Aluno não encontrado.']
+        });
+      }
+      return res.json(aluno);
+    }catch(e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message)
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if(!id) {
+        return res.status(401).json({
+          errors: ['ID não enviado.']
+        })
+      }
+
+      const alunoDeletado = await Aluno.findByPk(id);
+
+      if(!alunoDeletado) {
+        return res.status(401).json({
+          errors: ['Aluno não encontrado.']
+        })
+      }
+
+      alunoDeletado.destroy();
+      return res.json(alunoDeletado);
+    }catch(e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message)
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if(!id) {
+        return res.status(401).json({
+          errors: ['ID não enviado']
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if(!aluno) {
+        return res.status(401).json({
+          errors: ['Aluno não encontrado.']
+        });
+      }
+
+      await Aluno.update(req.body, {where: {
+        id: id
+      }}).then(async () => {
+        const novosDados = await Aluno.findByPk(id);
+          return res.json(novosDados);
+      })
+    }catch(e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message)
+      });
+    }
+  }
+
 }
 
 
--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -3,5 +3,9 @@ import alunoController from '../controllers/AlunoController';
 const router = new Router();
 
 router.get('/', alunoController.index);
+router.post('/', alunoController.store);
+router.get('/:id', alunoController.retrieve);
+router.put('/:id', alunoController.update);
+router.delete('/:id', alunoController.delete);
 
 export default router;
